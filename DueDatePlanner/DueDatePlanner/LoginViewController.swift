cbba43415e17ec90f46d33df49eedee6dfae09ca
Xusheng Fred Liu <liux6@rose-hulman.edu>
2020-05-06 13:35:38 -0400

    Finish most parts of UI layout

---
+++ DueDatePlanner/DueDatePlanner/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  DueDatePlanner
+//
+//  Created by Xusheng Liu on 5/6/20.
+//  Copyright © 2020 Rose-Hulman. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
