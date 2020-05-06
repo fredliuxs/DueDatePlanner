4e60b824f8da57ad49c0047d07bb0bdc826fe4e3
Xusheng Fred Liu <liux6@rose-hulman.edu>
2020-05-06 15:56:36 -0400

    Add due date object

---
+++ DueDatePlanner/DueDate.swift
@@ -0,0 +1,13 @@
+//
+//  DueDate.swift
+//  DueDatePlanner
+//
+//  Created by Xusheng Liu on 5/6/20.
+//  Copyright © 2020 Rose-Hulman. All rights reserved.
+//
+
+import UIKit
+
+class DueDate: NSObject {
+    
+}
