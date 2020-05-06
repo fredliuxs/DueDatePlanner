847e1674494d7d391e3a6c54fbad53ea81216707
Xusheng Fred Liu <liux6@rose-hulman.edu>
2020-05-06 14:21:44 -0400

    Tweak the rose-fire library to adapt it to Swift 5.0

--- DueDatePlanner/SceneDelegate.swift
+++ DueDatePlanner/SceneDelegate.swift
@@ -9,23 +9,23 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    let showLoginScreen = true
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
         self.window = self.window ?? UIWindow()
-        let showLoginScreen = true
         if showLoginScreen {
-          showLoginViewController();
+            showLoginViewController();
         } else {
-          showNavViewController();
+            showNavViewController();
         }
-
+        
         self.window!.makeKeyAndVisible()
         
         guard let _ = (scene as? UIWindowScene) else { return }
@@ -33,52 +33,52 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     @objc
     func handleLogin() {
-      print("TODO: Implement sign in from the AppDelegate")
+        print("TODO: Implement sign in from the SceneDelegate")
     }
-
+    
     @objc
     func handleLogout() {
-      print("TODO: Implement sign out from the AppDelegate")
+        print("TODO: Implement sign out from the SceneDelegate")
     }
     
     func showLoginViewController() {
-      let storyboard = UIStoryboard(name: "Main", bundle: nil)
-      window!.rootViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        window!.rootViewController = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
     }
-
+    
     func showNavViewController() {
-      let storyboard = UIStoryboard(name: "Main", bundle: nil)
-      window!.rootViewController = storyboard.instantiateViewController(withIdentifier: "NavViewController")
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        window!.rootViewController = storyboard.instantiateViewController(withIdentifier: "NavViewController")
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
+    
+    
 }
 
--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 #Uncomment the next line to define a global platform for your project
-platform :ios, '9.0'
+platform :ios, '11.0'
 
 target 'DueDatePlanner' do
   # Comment the next line if you don't want to use dynamic frameworks
@@ -7,5 +7,7 @@ target 'DueDatePlanner' do
 
   # Pods for DueDatePlanner
   pod 'Firebase'
+  pod 'Firebase/Auth'
   pod 'Firebase/Database'
+  pod 'Rosefire', :git => 'https://ada.csse.rose-hulman.edu/rosefire/ios-sdk.git'
 end
